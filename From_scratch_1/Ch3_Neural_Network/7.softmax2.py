7a727d3a79ea2582658ef9fbb0a9b488d13cf8be
DESKTOP-135RQ3N\namkukhyeon <ska5686a@gmail.com>
2025-01-04 17:28:16 +0900

    softmax 함수 구현. 출력 정규화에 좋을 것 같다.

---
+++ From_scratch_1/Ch3_Neural_Network/7.softmax2.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+a = np.array([1010, 1000, 990])
+np.exp(a) / np.sum(np.exp(a))
+c = np.max(a)
+a - c
+print(np.exp(a - c) / np.sum(np.exp(a - c)))
+
+# same as
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    
+    return y
+
+# Properties
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y)
+print(np.sum(y)) # 1
